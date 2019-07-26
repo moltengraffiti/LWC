f2b357a0fc11f6021d1e75ba2cf06a2a24396803
Sam Reilly <samreilly@gmail.com>
2019-07-26 16:30:50 +1000

    Basic conditional rendering

--- LWCUdacity/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ LWCUdacity/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,8 +2,12 @@ import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
 	@track dynamicGreeting = 'World';
+	@track displayDiv = false;
 
 	greetingChangeHandler(event) {
 		this.dynamicGreeting = event.target.value;
+	}
+	showDivHandler(event) {
+		this.displayDiv = event.target.checked;
 	}
 }
