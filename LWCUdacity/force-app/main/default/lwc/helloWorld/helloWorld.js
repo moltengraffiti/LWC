dbd78971f2750bdb8faa57e0f64a2a950ae81c64
Sam Reilly <samreilly@gmail.com>
2019-07-26 17:00:47 +1000

    Added ieration

--- LWCUdacity/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ LWCUdacity/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -3,6 +3,7 @@ import { LightningElement, track } from 'lwc';
 export default class HelloWorld extends LightningElement {
 	@track dynamicGreeting = 'World';
 	@track displayDiv = false;
+	@track cityList = [ 'Sydney', 'Dublin', 'London', 'Berlin' ];
 
 	greetingChangeHandler(event) {
 		this.dynamicGreeting = event.target.value;
